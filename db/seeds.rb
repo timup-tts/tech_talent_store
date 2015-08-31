b173a5e74d020c3e7c906a349a054ad177cc68f7
Tim Upchurch <mail@timup.ch>
2015-08-31 10:41:54 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = Category.create([{name: 'Gin'},{name: 'Vodka'},{name: 'Rum'},{name: 'Whiskey'},{name: 'Bourbon'},{name: 'Scotch'},{name: 'Liqueur'}])
