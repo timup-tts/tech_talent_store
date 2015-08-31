b16f40cebca47a15f7a825fef5814f8562d7e945
Tim Upchurch <mail@timup.ch>
2015-08-31 11:06:51 -0400

    add paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831142529) do
+ActiveRecord::Schema.define(version: 20150831144916) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -26,8 +26,12 @@ ActiveRecord::Schema.define(version: 20150831142529) do
     t.string   "brand"
     t.integer  "rating"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
