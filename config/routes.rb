3aba51854db93dc9dffef84e7914aa7c1c79eb65
Tim Upchurch <mail@timup.ch>
2015-08-31 10:30:32 -0400

    scaffolds and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'storefront/index'
+
+  resources :categories
+  resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ db/migrate/20150831142403_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.float :price
+      t.string :description
+      t.string :brand
+      t.integer :rating
+      t.integer :category_id
+
+      t.timestamps null: false
+    end
+  end
+end
